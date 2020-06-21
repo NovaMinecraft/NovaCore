f8f3622eef4716d5bee831af7662c564df6ad88e
RoyalScribblz <clashmonster123@gmail.com>
2020-06-21 15:41:21 +0100

    .

--- src/main/java/uk/co/novamc/novacore/commands/MainCommand.java
+++ src/main/java/uk/co/novamc/novacore/commands/MainCommand.java
@@ -1,5 +1,6 @@
 package uk.co.novamc.novacore.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,6 +14,10 @@ public class MainCommand implements CommandExecutor {
         this.plugin = plugin;
     }
 
+    private void sendMsg(CommandSender sender, String msg) {
+        sender.sendMessage((ChatColor.translateAlternateColorCodes('&', msg)));
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String string, String[] args) {
         if (args.length >=1) {
@@ -22,6 +27,12 @@ public class MainCommand implements CommandExecutor {
                 plugin.reloadConfig();
                 plugin.joinLeaveFile.reload();
                 plugin.scoreboardFile.reload();
+                plugin.paperCommandsFile.reload();
+                plugin.stackValueFile.reload();
+                plugin.iChestConfig.reload();
+                plugin.iChestDatabase.reload();
+                plugin.mobcoinConfig.reload();
+                sendMsg(sender, "&aSuccessfully reloaded!");
             }
         }
         return true;
